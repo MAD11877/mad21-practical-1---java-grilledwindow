2f94c9a5f2f52762e0ed9e60faf71ceaa3b5bcf5
Xavier Ang <xave@ctemplar.com>
2021-04-22 09:20:53 +0800

    I have no idea what's wrong

--- Question2.java
+++ Question2.java
@@ -19,6 +19,6 @@ public class Question2
     double weight = in.nextDouble();
 
     double bmi = weight / height / height;
-    System.out.format("Your BMI is %.1f\n", bmi);
+    System.out.format("%.1f\n", bmi);
   }
 }
